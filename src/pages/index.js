3dcc3e14a807c8dfb11266fafa01e9f892fe60c4
Juan Carlos Rodriguez <jcrodriguezam@outlook.es>
2021-01-12 16:18:56 +0100

    Fix descktop sizes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,27 @@
 import React from "react"
+import styled from 'styled-components'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import AppBody from '../components/Body'
 import "../styles/styles.css"
 
+const Wrapper = styled.div`
+  max-width: 768px;
+  overflow: hidden;
+  margin: 0 auto;
+  min-height: 100vh;
+  background: rgb(17,17,17);
+  background: linear-gradient(0deg, rgba(17,17,17,1) 0%, rgba(51,51,51,1) 100%);
+`;
+
 export default function Home() {
   return (
     <div className="body">
-      <Header />
-      <AppBody />
-      <Footer />
+      <Wrapper>
+        <Header />
+        <AppBody />
+        <Footer />
+      </Wrapper>
     </div>
   )
 }
